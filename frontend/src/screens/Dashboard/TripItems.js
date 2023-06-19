cec43748f788901f60e4338286c79a2a60971987
Rahul Saliya <RHS.Dev@outlook.com>
2023-06-19 01:09:51 -0300

    - Loaded dummy data from json.

---
+++ frontend/src/screens/Dashboard/TripItems.js
@@ -0,0 +1,53 @@
+const popularTrips = [
+    {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 1000,
+        rating: 4.9,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    }, {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 750,
+        rating: 4.8,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    }, {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 400,
+        rating: 4.5,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    },
+];
+
+const tripsNearYou = [
+    {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 1500,
+        rating: 4.1,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    }, {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 550,
+        rating: 4.3,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    }, {
+        id: 1,
+        title: "Halifax",
+        location: "Halifax, Nova Scotia",
+        cost: 1200,
+        rating: 4.2,
+        description: "Halifax, an Atlantic Ocean port in eastern Canada, is the provincial capital of Nova Scotia. A major business centre, it’s also known for its maritime history. The city’s dominated by the hilltop Citadel, a star-shaped fort completed in the 1850s. ",
+    }
+];
+
+
+
+export { popularTrips, tripsNearYou };

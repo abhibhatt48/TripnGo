cfe55786a0cd44af6ffec3b4151cb0d4e242144c
Abhishek Bhatt <47849440+abhibhatt48@users.noreply.github.com>
2023-07-19 13:51:34 -0300

    Added delete API for delete feedbacks.

--- backend/routes/contact_us.js
+++ backend/routes/contact_us.js
@@ -46,4 +46,22 @@ router.get('/', async function (req, res) {
   }
 });
 
+/* DELETE contact us information by id */
+router.delete('/:id', async function (req, res) {
+  const id = req.params.id;
+  const database = await db();
+
+  try {
+    const result = await database.collection('ContactUs').deleteOne({ _id: ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: 'Contact message not found.' });
+    }
+
+    res.json({ message: 'Contact message deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
 module.exports = router;

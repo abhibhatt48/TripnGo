7f2d98d47341bd72c586fa6c9eb442406cb4004b
kunapaneni <nv676973@dal.ca>
2023-07-21 00:59:56 -0300

    POST API for wishlist

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactUsRouter = require('./routes/contact_us');
 var paymentRouter = require('./routes/payment');
+const wishlistRouter = require('./routes/wishlist');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contact-us', contactUsRouter);
 app.use('/payment', paymentRouter);
+app.use('/wishlist', wishlistRouter);
 
 module.exports = app;
--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -19,3 +19,4 @@ router.get('/:id', function (req, res, next) {
 });
 
 module.exports = router;
+

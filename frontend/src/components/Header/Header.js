5e8b6027bfcb5c4d48c11e35aaaffc4bd8dd8600
Rahul Saliya <RHS.Dev@outlook.com>
2023-07-26 00:34:57 -0300

    - Added navigation to profile page.

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -230,20 +230,20 @@ const Header = () => {
 
           {
             userId && userId !== "undefined" &&
-            <div className="menu-item">
+            <div className="menu-item" onClick={() => {
+              showOptions && setShowOptions(false);
+              setCurrentKey("Profile");
+              navigate('/profilePage');
+            }}>
               <span
                 className={
                   "profile-text menu-button " +
                   (currentKey === "Profile" ? "active" : "")
                 }
-                onClick={() => {
-                  showOptions && setShowOptions(false);
-                  setCurrentKey("Profile");
-                }}
               >
                 Profile
               </span>
-              <div className="profile-container">
+              <div className="profile-container" >
                 <FaUser className="profile-icon" />
               </div>
             </div>

aeb070c846184b57a14f1e56ecc8fb311a0f6cb1
Naveen Kunapaneni <nv676973@dal.ca>
2023-08-07 20:00:49 -0300

    Updated backend api for wishlist

--- backend/routes/wishlist.js
+++ backend/routes/wishlist.js
@@ -5,6 +5,24 @@ const { ObjectId } = require('mongodb');
 
 
 /* POST wish list packages id */
+// router.post('/packages',  async(req, res) => {
+
+//     console.log("serverhit");
+//     const ids = req.body.id; // Get the array of ids from the request body
+//     console.log(ids);
+//     const database = await db();
+//     console.log("serverhit");
+//     try {
+//       const packageDetails = await database.collection('TravelPackages').find({ id: { $in: ids } }).toArray();
+//       if (!packageDetails || packageDetails.length === 0) {
+//         return res.status(404).json({ error: 'No such package found.' });
+//       }
+//       res.status(200).json(packageDetails)
+//     } catch (error) {
+//       res.status(500).json({ error: error.toString() });
+//     } 
+//   })
+/* POST wish list packages id */
 router.post('/packages',  async(req, res) => {
 
     console.log("serverhit");
@@ -13,7 +31,12 @@ router.post('/packages',  async(req, res) => {
     const database = await db();
     console.log("serverhit");
     try {
-      const packageDetails = await database.collection('TravelPackages').find({ id: { $in: ids } }).toArray();
+      const objectIds = ids.map(id => ObjectId(id));
+      const packageDetails = await database.collection('TravelPackages')
+        .find({ _id: { $in: objectIds } })
+        .toArray();
+
+      // const packageDetails = await database.collection('TravelPackages').find({ _id: ObjectId(ids) }).toArray();
       if (!packageDetails || packageDetails.length === 0) {
         return res.status(404).json({ error: 'No such package found.' });
       }
@@ -22,6 +45,5 @@ router.post('/packages',  async(req, res) => {
       res.status(500).json({ error: error.toString() });
     } 
   })
-
 
 module.exports = router
